048427a05483fd0798e34cc15f992471448956fb
MuhammedTafli <koska_33@hotmail.com>
2022-10-05 11:23:16 +0300

    Mükemmel sayıyı bulma projesi tamamlandı.

---
+++ mükemmelSayilar.java
@@ -0,0 +1,18 @@
+public class mükemmelSayilar {
+    public static void main(String[] args) {
+        // 6-> 1,2,3
+        int number = 26;
+        int total = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                total = total + i;
+            }
+        }
+        if (total == number) {
+            System.out.println("Mükemmel sayıdır.");
+        } else {
+            System.out.println("Mükemmel sayı değildir.");
+
+        }
+    }
+}
